a20fb2a822d4c8ef3f1a96f445bf385dd269ed81
a1tta6 <miniG@ET-PC>
2023-05-26 01:13:33 +0500

    Добавьте файлы проекта.

---
+++ StanOK/Authorization/View/LoginView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StanOK.Authorization.View
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainPage.View.MainPageView mainPageView = new MainPage.View.MainPageView();
+            mainPageView.Show();
+        }
+    }
+}
