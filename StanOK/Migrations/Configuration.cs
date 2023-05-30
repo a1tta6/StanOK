96de7b003992e459c5a1e6b4ce87d3193882d3bd
a1tta6 <miniGAN22@gmail.com>
2023-05-30 12:57:40 +0500

    Частичное заполнение тестовыми данными

--- StanOK/Migrations/Configuration.cs
+++ StanOK/Migrations/Configuration.cs
@@ -1,5 +1,7 @@
 ﻿namespace StanOK.Migrations
 {
+    using StanOK.Authorization.Model;
+    using StanOK.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,6 +16,20 @@
 
         protected override void Seed(StanOK.Utils.UserContext context)
         {
+            context.Users.Add(new LoginModel { Login = "TEST", Role = "TEST", Password = "TEST" });
+            context.RepairTypes.Add(
+                new RepairTypeModel
+                {
+                    Id = 1,
+                    Repair_name = "Мелкий ремонт",
+                    Cost = 5600,
+                    Comment = "Ремонт малой продолжительности, c которым может справиться один мастер в течение рабочей смены",
+                    Duration = DateTime.Parse("00.00.00 4:00")
+                }
+            );
+            context.Machines.Add(new MachineModel { Id = 0, Year = DateTime.UtcNow, Brand = "KSKSK", MachineType = "sada", Country = "afasf" });
+            context.Repairs.Add(new RepairModel { Id = 0, MachineType = 0, Comment = "safas", DateBegin = DateTime.UtcNow, RepairType = 0 });
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
--- StanOK/Utils/UserContext.cs
+++ StanOK/Utils/UserContext.cs
@@ -24,6 +24,21 @@ namespace StanOK.Utils
         {
             modelBuilder.HasDefaultSchema("public");
             base.OnModelCreating(modelBuilder);
+
+            //Users.Add(new LoginModel { Login = "TEST", Role = "TEST", Password = "TEST" });
+            //RepairTypes.Add(
+            //    new RepairTypeModel
+            //    {
+            //        Id = 0,
+            //        Repair_name = "Мелкий ремонт",
+            //        Cost = 300,
+            //        Comment = "Ремонт малой продолжительности",
+            //        Duration = DateTime.UtcNow
+            //    }
+            //);
+            //Machines.Add(new MachineModel { Id = 0, Year = DateTime.UtcNow, Brand = "KSKSK", MachineType = "sada", Country = "afasf" });
+            //Repairs.Add(new RepairModel { Id = 0, MachineType = 0, Comment = "safas", DateBegin = DateTime.UtcNow, RepairType = 0 });
+
         }
     }
 }
